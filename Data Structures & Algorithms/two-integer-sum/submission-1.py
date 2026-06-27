8b30801ae49698558b80438eb36a0818a9b2b74b
Marlin Ranasinghe <77016115+MarlzRana@users.noreply.github.com>
2026-06-27 12:29:54 +0100

    Add: two-integer-sum - submission-1

---
+++ Data Structures & Algorithms/two-integer-sum/submission-1.py
@@ -0,0 +1,16 @@
+# Time complexity: O(n)
+# Space complexity: O(n)
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        nums_idx = {}
+
+        for idx, num in enumerate(nums):
+            complement = target - num
+            if complement in nums_idx:
+                return [nums_idx[complement], idx]
+
+            nums_idx[num] = idx
+
+        raise Exception("There should be two indices from nums that sum to make target")
+        
